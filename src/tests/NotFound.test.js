f897ce291c57261a5a914ec4339a05e5734a7b0f
joabe <joabe9999pereira@gmail.com>
2022-10-22 21:55:34 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import { screen, render } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+describe('test the not found Component', () => {
+  it('test if the Component contains a heading level 2 with the text: "page requested not found"', () => {
+    render(<NotFound />);
+
+    const notFoundH2 = screen.getByRole('heading', { name: /Page requested not found/i, level: 2 });
+
+    expect(notFoundH2).toBeDefined();
+  });
+
+  it('test if the Component displays the correct image', () => {
+    render(<NotFound />);
+
+    const image = screen.queryByAltText(/Pikachu crying because the page requested was not found/i);
+    const imageUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const { src } = image;
+
+    expect(image).toBeDefined();
+
+    expect(src).toBe(imageUrl);
+  });
+});
